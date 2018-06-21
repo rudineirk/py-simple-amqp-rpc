81dea14fb060a21c2791520544b3dc59828ff4c7
Rudinei Goi Roecker <rudineigr@itflex.com.br>
2018-06-21 09:31:31 -0300

    release: 0.3.2

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(basedir, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='simple-amqp-rpc',
-    version='0.3.1',
+    version='0.3.2',
     description='Simple AMQP RPC lib',
     long_description=long_description,
     long_description_content_type='text/markdown',
